66c7d73bacbcbeeb3a6820ba35fa808b247b9c35
nurselzengin <125567986+nurselzengin@users.noreply.github.com>
2023-11-06 18:07:33 +0300

    Update Sql_Samples.sql

--- Sql_Samples.sql
+++ Sql_Samples.sql
@@ -1,5 +1,10 @@
+--ODEV-1
 --SELECT title, description FROM film;
 --SELECT * FROM film WHERE length >60 AND length<75;
 --SELECT * FROM film WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 --SELECT * FROM customer WHERE first_name = 'Mary'; sorgusu sonucunda elde ettiğimiz bilgiye göre sorunun cevabı "Smith"tir.
 --SELECT * FROM film WHERE length<50 AND NOT(rental_rate = 2.99 OR rental_rate = 4.99);
+--ODEV-2
+--SELECT replacement_cost FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99 ;
+--SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope', 'Nick', 'Ed');
+--SELECT rental_rate, replacement_cost FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
